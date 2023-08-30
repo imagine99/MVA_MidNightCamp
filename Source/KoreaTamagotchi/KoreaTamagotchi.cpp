ca7fa4d84a192aad1378e2b75906bca9b418009c
imagine99 <57169754+imagine99@users.noreply.github.com>
2023-08-30 16:22:10 +0900

    First Push
    Create Empty Project c++
    Import ThirdPersonBP
    Create LHJ Folder

---
+++ Source/KoreaTamagotchi/KoreaTamagotchi.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KoreaTamagotchi.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KoreaTamagotchi, "KoreaTamagotchi" );
