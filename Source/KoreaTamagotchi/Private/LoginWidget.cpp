ac5b71a6a6ab894a3769aa6e7346645edebe0498
imagine99 <57169754+imagine99@users.noreply.github.com>
2023-08-30 20:01:25 +0900

    LoginWidget Design and value bind

--- Source/KoreaTamagotchi/Private/LoginWidget.cpp
+++ Source/KoreaTamagotchi/Private/LoginWidget.cpp
@@ -2,4 +2,17 @@
 
 
 #include "LoginWidget.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Components/EditableText.h"
 
+void ULoginWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+ //login session을 웹서버에 보낼 것이다.
+}
+
+void ULoginWidget::SendLogin()
+{
+
+}
--- Source/KoreaTamagotchi/Public/LoginWidget.h
+++ Source/KoreaTamagotchi/Public/LoginWidget.h
@@ -13,5 +13,37 @@ UCLASS()
 class KOREATAMAGOTCHI_API ULoginWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+
+protected:
+	virtual void NativeConstruct() override;
+
+	/*-------------- ID ------------------*/
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget), Category = MySettings)
+	class UTextBlock* text_ID;
 	
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget), Category = MySettings)
+	class UEditableText* editable_ID;
+	
+	/*-------------- PW ------------------*/
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget), Category = MySettings)
+	class UTextBlock* text_PW;
+
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget), Category = MySettings)
+	class UEditableText* editable_PW;
+	
+	/*---------- Login BTN -----------*/
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget), Category = MySettings)
+	class UButton* btn_Login;
+	
+	/*--------IMAGE-------*/
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget), Category = MySettings)
+	class UImage* Img_Login;
+
+
+
+
+	/*----------------------------------FUNCTION----------*/
+	UFUNCTION()
+	void SendLogin();
 };
