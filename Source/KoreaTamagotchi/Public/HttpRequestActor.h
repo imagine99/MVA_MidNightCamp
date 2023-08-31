ab25a653961a21033f725d02e79061fc8dd465dc
imagine99 <57169754+imagine99@users.noreply.github.com>
2023-08-31 21:31:48 +0900

    Json Login_singin_middle save

---
+++ Source/KoreaTamagotchi/Public/HttpRequestActor.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HttpRequestActor.generated.h"
+
+UCLASS()
+class KOREATAMAGOTCHI_API AHttpRequestActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHttpRequestActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	//#1. GET -> server에서 데이터 받아오기 함수
+	//void SendRequest(const FString url);
+
+private:
+	//void OnReceivedData(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bConnectedSuccessfully);
+	
+	UPROPERTY()
+	class ALoginGameModeBase* gm ;
+
+};
+	
